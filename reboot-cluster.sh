810bb4694473ce6b3eae6b2c0dd4b16340a70281
Tim Carr <tim@timcarr.net>
2019-05-28 13:12:51 -0500

    fixed some of the waits

--- reboot-cluster.sh
+++ reboot-cluster.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
 # Default values
-
 dc_name="lab"
 cluster_name="nuc"
 
@@ -9,22 +8,27 @@ cluster_name="nuc"
 cluster_path="/$dc_name/host/$cluster_name"
 
 for host in `govc find $cluster_path -type h`; do
-
-#TODO check to make sure that no hosts are in maintenance mode
-
 #enter maintenance mode
+  echo "Placing $host in maintenance mode"
   govc host.maintenance.enter $host
-
-#TODO verify that the host is in maintenance mode
-
 #shutdown the host
-#govc host.shutdown -r=true -host.ip=192.168.10.85
+  echo "Restarting $host"
   govc host.shutdown -r=true $host
 #wait for host to report as unresponsive to vCenter
-  sleep 300
+  conn_state=$(govc object.collect -json $host | jq -r '.[28] .Val .ConnectionState')
+  while [ $conn_state != "notResponding" ]; do
+    echo "Waiting for the esxi host $host to stop responding to vCenter - sleeping for 10s"
+    sleep 10
+    conn_state=$(govc object.collect -json $host | jq -r '.[28] .Val .ConnectionState')
+  done
+  echo "exited the loop"
 #wait for the host to come back online in vCenter
-
+  while [ $conn_state != "connected" ]; do
+    echo "Waiting for the esxi host $host to come back online in vCenter - sleeping for 10s"
+    sleep 10
+    conn_state=$(govc object.collect -json $host | jq -r '.[28] .Val .ConnectionState')
+  done
 #exit from maintenance mode
-#govc host.maintenance.exit -host.ip=192.168.10.85
+  echo "Exiting $host from maintenance mode"
   govc host.maintenance.exit $host
 done
